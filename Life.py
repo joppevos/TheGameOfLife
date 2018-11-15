8ac8468dd1600a3b3062b0a812d1d8685d6e3787
joppevos <joppe.vos@gmail.com>
2018-11-15 17:46:09 +0100

    added diehard seed

--- Life.py
+++ Life.py
@@ -13,13 +13,58 @@ turns OFF.
 4. If a cell is OFF and has exactly three neighbors that are ON, it .
 turns ON.
 """
+# seeds = {
+#     "diehard": [
+#         [0, 0, 0, 0, 0, 0, 1, 0],
+#         [1, 1, 0, 0, 0, 0, 0, 0],
+#         [0, 1, 0, 0, 0, 1, 1, 1],
+#     ],
+#     "boat": [[1, 1, 0], [1, 0, 1], [0, 1, 0]],
+#     "r_pentomino": [[0, 1, 1], [1, 1, 0], [0, 1, 0]],
+#     "pentadecathlon": [
+#         [1, 1, 1, 1, 1, 1, 1, 1],
+#         [1, 0, 1, 1, 1, 1, 0, 1],
+#         [1, 1, 1, 1, 1, 1, 1, 1],
+#     ],
+#     "beacon": [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]],
+#     "acorn": [[0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [1, 1, 0, 0, 1, 1, 1]],
+#     "spaceship": [[0, 0, 1, 1, 0], [1, 1, 0, 1, 1], [1, 1, 1, 1, 0], [0, 1, 1, 0, 0]],
+#     "block_switch_engine": [
+#         [0, 0, 0, 0, 0, 0, 1, 0],
+#         [0, 0, 0, 0, 1, 0, 1, 1],
+#         [0, 0, 0, 0, 1, 0, 1, 0],
+#         [0, 0, 0, 0, 1, 0, 0, 0],
+#         [0, 0, 1, 0, 0, 0, 0, 0],
+#         [1, 0, 1, 0, 0, 0, 0, 0],
+#     ],
+#     "infinite": [
+#         [1, 1, 1, 0, 1],
+#         [1, 0, 0, 0, 0],
+#         [0, 0, 0, 1, 1],
+#         [0, 1, 1, 0, 1],
+#         [1, 0, 1, 0, 1],
+#     ],
+# }
+
+# TODO: ADD SEEDS IN A DIC TO CHOOSE FROM
+# TODO: CALL KEY AND PLACE VALUE IN GRID
 
 def randomgrid(n):
  """returns a grid of NxN random values"""
  return np.random.choice([1,0], n*n, p=[0.2, 0.8]).reshape(n, n)
 
 
-def addglider(i,j, grid):
+def diehard(i,j,grid):
+    diehard = np.array([[0, 0, 0, 0, 0, 0, 1, 0],
+                        [1, 1, 0, 0, 0, 0, 0, 0],
+                        [0, 1, 0, 0, 0, 1, 1, 1]])
+
+    grid[i:i + 3, j:j + 8] = diehard
+    return grid
+
+
+def glider(i,j, grid):
+
     """ a glider seed to add to the grid universe
     :param i is x position on grid
     :param j is y position on grid
@@ -58,7 +103,7 @@ def update(frameNum, img, grid, n):
                     newgrid[i, j] = 0
             else:
                 if total == 3:
-                    newgrid[i,j] = 1
+                    newgrid[i, j] = 1
         # update data
     img.set_data(newgrid)
     grid[:] = newgrid[:]
@@ -66,20 +111,26 @@ def update(frameNum, img, grid, n):
 
 
 def main():
+    # set parameters
+    color = 'Blues'
     # size of the grid
     n = 50
-    glider = False
-    if glider:
+    seed = 'diehard'
+    if seed == 'glider':
         zeros = np.zeros((n, n))
-        grid = addglider(30, 30, zeros)
+        grid = glider(30, 30, zeros)
+    elif seed == 'diehard':
+        zeros = np.zeros((n, n))
+        grid = diehard(30, 30, zeros)
+
     else:
         grid = randomgrid(n)
 
     # set up animation
     fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation='nearest', cmap='Purples')
+    img = ax.imshow(grid, interpolation='nearest', cmap=color)
     ani = animation.FuncAnimation(fig, update, fargs=(img, grid, n,),
-                                  # frames=1,
+                                  frames=25,
                                   interval=100,
                                   save_count=50)
 
