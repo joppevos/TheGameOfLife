c5a9681aaead681f2c1318b9125f0dd934a1f0fa
DESKTOP-HH5OSHI\Joppe <joppe.vos@gmail.com>
2018-11-15 20:28:26 +0100

    added loads of seeds

--- Life.py
+++ Life.py
@@ -13,69 +13,57 @@ turns OFF.
 4. If a cell is OFF and has exactly three neighbors that are ON, it .
 turns ON.
 """
-# seeds = {
-#     "diehard": [
-#         [0, 0, 0, 0, 0, 0, 1, 0],
-#         [1, 1, 0, 0, 0, 0, 0, 0],
-#         [0, 1, 0, 0, 0, 1, 1, 1],
-#     ],
-#     "boat": [[1, 1, 0], [1, 0, 1], [0, 1, 0]],
-#     "r_pentomino": [[0, 1, 1], [1, 1, 0], [0, 1, 0]],
-#     "pentadecathlon": [
-#         [1, 1, 1, 1, 1, 1, 1, 1],
-#         [1, 0, 1, 1, 1, 1, 0, 1],
-#         [1, 1, 1, 1, 1, 1, 1, 1],
-#     ],
-#     "beacon": [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]],
-#     "acorn": [[0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [1, 1, 0, 0, 1, 1, 1]],
-#     "spaceship": [[0, 0, 1, 1, 0], [1, 1, 0, 1, 1], [1, 1, 1, 1, 0], [0, 1, 1, 0, 0]],
-#     "block_switch_engine": [
-#         [0, 0, 0, 0, 0, 0, 1, 0],
-#         [0, 0, 0, 0, 1, 0, 1, 1],
-#         [0, 0, 0, 0, 1, 0, 1, 0],
-#         [0, 0, 0, 0, 1, 0, 0, 0],
-#         [0, 0, 1, 0, 0, 0, 0, 0],
-#         [1, 0, 1, 0, 0, 0, 0, 0],
-#     ],
-#     "infinite": [
-#         [1, 1, 1, 0, 1],
-#         [1, 0, 0, 0, 0],
-#         [0, 0, 0, 1, 1],
-#         [0, 1, 1, 0, 1],
-#         [1, 0, 1, 0, 1],
-#     ],
-# }
 
-# TODO: ADD SEEDS IN A DIC TO CHOOSE FROM
-# TODO: CALL KEY AND PLACE VALUE IN GRID
+# seed location's from different sources
+seeds = {
+    "diehard": [
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 1, 1],
+    ],
+    "boat": [[1, 1, 0], [1, 0, 1], [0, 1, 0]],
+    "r_pentomino": [[0, 1, 1], [1, 1, 0], [0, 1, 0]],
+    "pentadecathlon": [
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+    "beacon": [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]],
+    "acorn": [[0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0], [1, 1, 0, 0, 1, 1, 1]],
+    "spaceship": [[0, 0, 1, 1, 0], [1, 1, 0, 1, 1], [1, 1, 1, 1, 0], [0, 1, 1, 0, 0]],
+    "block_switch_engine": [
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0, 1, 1],
+        [0, 0, 0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0],
+    ],
+    "infinite": [
+        [1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1],
+        [0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1],
+    ],
+}
 
 def randomgrid(n):
  """returns a grid of NxN random values"""
  return np.random.choice([1,0], n*n, p=[0.2, 0.8]).reshape(n, n)
 
 
-def diehard(i,j,grid):
-    diehard = np.array([[0, 0, 0, 0, 0, 0, 1, 0],
-                        [1, 1, 0, 0, 0, 0, 0, 0],
-                        [0, 1, 0, 0, 0, 1, 1, 1]])
-
-    grid[i:i + 3, j:j + 8] = diehard
-    return grid
-
-
-def glider(i,j, grid):
-
+def seed_placer(i, j, grid,seed):
     """ a glider seed to add to the grid universe
     :param i is x position on grid
     :param j is y position on grid
     :param grid is the given universe
+    :param seed is type of seed array
     :return grid with seed glider placed
     """
+    shape = np.shape(seed)
 
-    glider = np.array([[0, 0, 1],
-                       [1, 0, 1],
-                       [0, 1, 1]])
-    grid[i:i+3, j:j+3] = glider
+    grid[i:i + shape[0], j:j + shape[1]] = seed
     return grid
 
 
@@ -110,34 +98,30 @@ def update(frameNum, img, grid, n):
     return img,
 
 
-def main():
+def main(seeds):
     # set parameters
-    color = 'Blues'
+    color = 'Oranges'
     # size of the grid
-    n = 50
-    seed = 'diehard'
-    if seed == 'glider':
-        zeros = np.zeros((n, n))
-        grid = glider(30, 30, zeros)
-    elif seed == 'diehard':
-        zeros = np.zeros((n, n))
-        grid = diehard(30, 30, zeros)
-
+    n = 30
+    strseed = 'pentadecathlon'
+    v = seeds.get(strseed)
+    if v != None:
+        grid = np.zeros((n, n))
+        seed_placer(13, 11, grid, v)
     else:
         grid = randomgrid(n)
 
     # set up animation
     fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation='nearest', cmap=color)
+    img = ax.imshow(grid, interpolation='nearest', cmap='Purples')
     ani = animation.FuncAnimation(fig, update, fargs=(img, grid, n,),
                                   frames=25,
-                                  interval=100,
+                                  interval=500,
                                   save_count=50)
-
     plt.show()
 
 
-main()
+main(seeds)
 
 
 
