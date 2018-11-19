979d37107d6e8e9e62500e10c67fd95d7029a2cd
joppevos <joppe.vos@gmail.com>
2018-11-19 15:18:02 +0100

    added: C4D 2D game of life version

--- c4d_Scratch.py
+++ c4d_Scratch.py
@@ -2,19 +2,34 @@ import c4d
 import numpy as np
 
 
-def execute():
-    n = 10
-    grid = np.random.choice([1, 0], n * n).reshape(10, 10)
+# Welcome to the world of Python
+
+
+class Particle:
+
+    def __init__(self):
+        self.tp = doc.GetParticleSystem()
+
+    def creatept(self):
+        self.p = self.tp.AllocParticle()
+
+    def move(self, vec):
+        self.tp.SetPosition(self.p, vec)
+
+def generate(particle, grid, n, y):
+    """
+    Calculates 1 generation
+    :param particle: particle instance from class Particle
+    :param grid: the grid from the generation before
+    :param n: size of the grid in n
+    :return: new generation grid
+    """
     copygrid = grid.copy()
-    y = 0
+    tp = doc.GetParticleSystem()
     for i in range(n):
         for j in range(n):
-            if grid[i, j] == 1:
-                vec = c4d.Vector(i * 230, j * 230, y)
-                obj = c4d.BaseObject(c4d.Ocube)  # Create new cube
-                obj.SetRelPos(c4d.Vector(vec))  # Set position of cube
-                doc.InsertObject(obj)  # Insert object in document
 
+            particle.creatept()
             total = int((grid[i, (j - 1) % n] + grid[i, (j + 1) % n] +
                          grid[(i - 1) % n, j] + grid[(i + 1) % n, j] +
                          grid[(i - 1) % n, (j - 1) % n] + grid[(i - 1) % n, (j + 1) % n] +
@@ -23,16 +38,50 @@ def execute():
             if grid[i, j] == 1:
                 if (total < 2) or (total > 3):
                     copygrid[i, j] = 0
+
             else:
                 if total == 3:
                     copygrid[i, j] = 1
-            y = 250
-            grid[:] = copygrid[:]
 
 
-execute()
+            if grid[i, j] == 1:
+
+                vec = c4d.Vector(i * 230, j * 230, y)
+                particle.move(vec)
+
+    grid[:] = copygrid[:]
+
+    return grid
+
+class Render:
+    n = 20
+    y = 200
+    def __init__(self):
+        self.grid = randomgrid(self.n)
+        self.tp = doc.GetParticleSystem()
+
+    def remove(self):
+        self.tp.FreeAllParticles()
+
+    def move(self, vec):
+        self.tp.SetPosition(self.p, vec)
+
+    def update(self):
+        #self.tp.FreeAllParticles()
+        p = Particle()
+        newgrid = generate(p, self.grid, self.n, self.y)
+        self.grid = newgrid
+        self.y += 200
 
 
+def randomgrid(n):
+    """returns a grid of NxN random values"""
+    return np.random.choice([1, 0], n * n, p=[0.2, 0.8]).reshape(n, n)
 
 
+instance = Render()
 
+def main():
+
+    if frame % 3 == 0:
+        instance.update()
